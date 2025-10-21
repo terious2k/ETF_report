52f9cba611f4f3911bba02aacd6356e73e34d0c0
terious2k <terious.kr@gmail.com>
2025-10-21 11:18:00 +0900

    Fix API URL for KRX data service

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import calendar
 
 # --- KRX API 정보 설정 ---
 # st.secrets에서 정보를 불러옵니다. (Streamlit Cloud의 Secrets 설정이 필수!)
-API_URL = 'https://data-dbg.krx.co.kr/svc/sample/apis/etp/etf_bydd_trd'
+API_URL = 'https://data-dbg.krx.co.kr/svc/apis/etp/etf_bydd_trd'
 
 try:
     API_ID = st.secrets["krx_api"]["api_id"]
